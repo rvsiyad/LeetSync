9073b304849bcdab437e4e1e9f8b1eae3e509240
Roble <roblesiyad123@gmail.com>
2024-09-12 17:06:17 +0100

    Time: 39 ms (39.72%) | Memory: 16.6 MB (31.82%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,35 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        
+        if numRows == 1:
+            return [[1]]
+        
+        if numRows == 2:
+            return [[1], [1,1]]
+        
+        rows = 2
+
+        res = [[1], [1,1]]
+    
+        while rows < numRows:
+            lastRow = res[-1]
+
+            newRow = []
+
+            L = 0
+            R = 1
+
+            newRow.append(1)
+            while R < len(lastRow):
+                newRow.append(lastRow[L] + lastRow[R])
+                R += 1
+                L += 1
+            
+            newRow.append(1)
+
+            res.append(newRow)
+            rows += 1
+
+        return res
