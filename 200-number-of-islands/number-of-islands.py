a48d1fca155a6d41396bf7ba498d94b3f17ce8d1
Roble <roblesiyad123@gmail.com>
2024-11-26 21:21:46 +0000

    Time: 242 ms (55.76%) | Memory: 19.5 MB (42.35%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,24 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(row, columns):
-            ROWS, COLUMNS = len(grid) - 1, len(grid[0]) - 1
+        num_islands = 0
+        ROWS = len(grid)
+        COLS = len(grid[0])
 
-            if row < 0 or row > ROWS or columns < 0 or columns > COLUMNS or grid[row][columns] != '1':
+        def remove_island(row, col):
+            if row < 0 or row >= ROWS or col < 0 or col >= COLS or grid[row][col] == "0":
                 return
-            else:
-                grid[row][columns] = '0'
-                dfs(row + 1, columns)
-                dfs(row - 1, columns)
-                dfs(row, columns + 1)
-                dfs(row, columns - 1)
-        
-        
-        numberOfIslands = 0
+            
+            if grid[row][col] == "1":
+                grid[row][col] = "0"
+            
+            remove_island(row + 1, col)
+            remove_island(row - 1, col)
+            remove_island(row, col + 1)
+            remove_island(row, col - 1)
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    numberOfIslands += 1
-                    dfs(i, j)
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == "1":
+                    num_islands += 1
+                    remove_island(row, col)
         
-        return numberOfIslands
+        return num_islands
