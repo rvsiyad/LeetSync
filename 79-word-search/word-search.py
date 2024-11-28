a76e4f77efa0e3f69efb1a3134865d9591993eca
Roble <roblesiyad123@gmail.com>
2024-11-28 12:05:21 +0000

    Time: 4385 ms (41.60%) | Memory: 17.5 MB (11.05%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,26 +1,30 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rowLength = len(board)
-        columnLength = len(board[0])
+        ROWS, COLS = len(board), len(board[0])
         path = set()
 
-        def backtrack(r,c,i):
-            if i == len(word):
+        if not word:
+            return True
+        
+        def backtrack(r, c, index, path):
+            if index >= len(word):
                 return True
             
-            if r >= rowLength or c >= columnLength or r < 0 or c < 0 or (r,c) in path or word[i] != board[r][c]:
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c] != word[index] or (r, c) in path:
                 return False
+
+            path.add((r, c))
             
-            path.add((r,c))
+            result = backtrack(r + 1, c, index + 1, path) or backtrack(r - 1, c, index + 1, path) or backtrack(r, c + 1, index + 1, path) or backtrack(r, c - 1, index + 1, path)
 
-            result = backtrack(r + 1, c, i + 1) or backtrack(r - 1, c, i + 1) or backtrack(r, c + 1, i + 1) or backtrack(r, c - 1, i + 1)
-            path.remove((r,c))
-
+            path.remove((r, c))
             return result
+            
         
-        for R in range(rowLength):
-            for C in range(columnLength):
-                if backtrack(R,C,0):
-                    return True
+        for row in range(ROWS):
+            for col in range(COLS):
+                if board[row][col] == word[0]:
+                    if backtrack(row, col, 0, path):
+                        return True
         
         return False
