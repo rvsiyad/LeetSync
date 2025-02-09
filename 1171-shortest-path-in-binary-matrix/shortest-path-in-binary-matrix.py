c490823cecc57b3fd9b9a8e3ace5745af81ca9b0
Roble <roblesiyad123@gmail.com>
2025-02-09 20:40:33 +0000

    Time: 149 ms (79.70%) | Memory: 18.2 MB (88.65%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,35 +1,37 @@
+from collections import deque
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        ROWS, COLUMNS = len(grid), len(grid[0])
+        ROWS = len(grid)
+        COLS = len(grid[0])
 
-        if grid[0][0] == 1 or grid[ROWS -1][COLUMNS -1] == 1:
+        if grid[0][0] == 1 or grid[ROWS - 1][COLS-1] == 1:
             return -1
 
-        queue = collections.deque()
-        queue.append((0,0))
+        queue = deque()
 
+        queue.append((0, 0))
         grid[0][0] = 1
 
         length = 1
 
         while queue:
-            for i in range(len(queue)):
-                r, c = queue.popleft()
+            for _ in range(len(queue)):
+                row, col = queue.popleft()
 
-                if r == ROWS -1 and c == COLUMNS -1:
+                if row == ROWS - 1 and col == COLS - 1:
                     return length
-
-                directions = [[0,1],[0,-1], [1,0], [-1,0], [1,1], [1,-1], [-1, 1], [-1,-1]]
+                
+                directions = [[0, 1], [1, 0], [-1, 0], [0, -1], [-1, 1], [1, -1], [-1, -1], [1, 1]]
 
                 for dr, dc in directions:
-                    newRow = r + dr
-                    newColumn = c + dc
+                    newRow, newCol = row + dr, col + dc
 
-                    if newRow < 0 or newColumn < 0 or newRow == ROWS or newColumn == COLUMNS or grid[newRow][newColumn] == 1:
+                    if newRow < 0 or newRow >= ROWS or newCol < 0 or newCol >= COLS or grid[newRow][newCol] == 1:
                         continue
                     
-                    grid[newRow][newColumn] = 1
-                    queue.append((newRow, newColumn))
-
+                    grid[newRow][newCol] = 1
+                    queue.append((newRow, newCol))
+            
             length += 1
+        
         return -1
