54735974d39afb31e520bc95f615275694b56d87
Roble <roblesiyad123@gmail.com>
2025-02-11 16:00:29 +0000

    Time: 81 ms (15.62%) | Memory: 18.3 MB (38.35%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -1,20 +1,27 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        count = {}
-        maxLength = float("-inf")
-
+        # Sliding window
+        """
+        We want to add to the sliding window will the number of zeros in the string is < k
+        Since the numbers are 0 and 1, we can keep the count using an array, where the index represents the number
+        We add to the right and remove from the left
+        Use a while loop to keep removing from the L
+        Use a two pointer to remove from L
+        """
+        count = [0, 0]
         L = 0
-
+        maxLength = float("-inf")
+        
         for R in range(len(nums)):
-            count[nums[R]] = 1 + count.get(nums[R], 0)
+            # Increment num count by 1
+            count[nums[R]] += 1
 
-            while count.get(0, 0) > k:
+            while count[0] > k:
+                # Remove elements from the left
                 count[nums[L]] -= 1
                 L += 1
             
             maxLength = max(maxLength, R - L + 1)
         
-        return maxLength
-            
-
+        return 0 if maxLength == float("-inf") else maxLength
 
