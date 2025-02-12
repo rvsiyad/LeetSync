993c087c16cd96f032951b6b21877f2761a06f41
Roble <roblesiyad123@gmail.com>
2025-02-12 11:21:17 +0000

    Time: 0 ms (100.00%) | Memory: 17.9 MB (16.53%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/valid-word-abbreviation.py
@@ -1,31 +1,32 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        # Two pointer, check values is not between ord('0') and ord('9') if not, check if same, if not same, return False
-        # If is a number, do a while loop, 
+        if (word and not abbr) or (abbr and not word):
+            return False
 
-        wordP, abbrP = 0, 0
+        wP, aP = 0, 0
 
-        while wordP < len(word) and abbrP < len(abbr):
-            if abbr[abbrP].isdigit():
+        while wP < len(word) and aP < len(abbr):
+            if abbr[aP].isdigit():
                 num = 0
 
-                if abbr[abbrP] == '0':
+                if abbr[aP] == '0':
                     return False
-
-                while abbrP < len(abbr) and abbr[abbrP].isdigit():
-                    num = num * 10 + int(abbr[abbrP])
-
-                    abbrP += 1
                 
-                wordP += num
+                while aP < len(abbr) and abbr[aP].isdigit():
+                    num = num * 10 + int(abbr[aP])
+
+                    aP += 1
+                
+                wP += num
+
             else:
-                if word[wordP] != abbr[abbrP]:
+                if word[wP] != abbr[aP]:
                     return False
-                
-                wordP += 1
-                abbrP += 1
-
-        if wordP == len(word) and abbrP == len(abbr):
-            return True
+            
+                aP += 1
+                wP += 1
         
-        return False
+        return wP == len(word) and aP == len(abbr)
+
+                
+
