28ba286e7fa08b25d67f93a40422002529b401e1
Roble <roblesiyad123@gmail.com>
2025-02-12 14:34:20 +0000

    Time: 99 ms (48.90%) | Memory: 26.9 MB (31.91%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Left pointer we move when we find a smaller starting price on the right pointer
-        # Each time we encounter a value, we take the max (diff)
+        greatestProfit = 0
         L = 0
-        maxProfit = float("-inf")
 
         for R in range(1, len(prices)):
-            if prices[R] < prices[L]:
+            if prices[L] > prices[R]:
                 L = R
-                continue
-            
-            maxProfit = max(maxProfit,prices[R] - prices[L])
-        
-        return 0 if maxProfit == float("-inf") else maxProfit
+            else:
+                greatestProfit = max(greatestProfit, prices[R] - prices[L])
+
+        return greatestProfit
