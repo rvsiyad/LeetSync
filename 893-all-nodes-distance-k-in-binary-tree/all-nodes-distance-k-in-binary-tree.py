30bc64a60ac6f4ae7231dec9f30ff8959c6a9c18
Roble <roblesiyad123@gmail.com>
2025-02-13 16:56:31 +0000

    Time: 30 ms (98.54%) | Memory: 18.3 MB (26.01%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,79 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        if k == 0:
+            return [target.val]
+
+        # We need to find all nodes connected to the current node.
+        # We can do this by creating an adjList of the currentNode and its connections
+        connectedNodes = collections.defaultdict(list)
+        queue = collections.deque()
+
+        queue.append(root)
+
+        while queue:
+            node = queue.popleft()
+
+            if node.left:
+                connectedNodes[node].append(node.left)
+                connectedNodes[node.left].append(node)
+
+                queue.append(node.left)
+
+            if node.right:
+                connectedNodes[node].append(node.right)
+                connectedNodes[node.right].append(node)
+
+                queue.append(node.right)
+
+
+        # DFS to create the adjList and then BFS to keep to distance of k?
+        def dfs(node):
+            if not node:
+                return 
+            
+            if node.left:
+                connectedNodes[node].append(node.left)
+                connectedNodes[node.left].append(node)
+                dfs(node.left)
+            
+            if node.right:
+                connectedNodes[node].append(node.right)
+                connectedNodes[node.right].append(node)
+                dfs(node.right)
+        
+        dfs(root)
+        # Now we have all the nodes, we need to BFS to k distance, add those nodes to result, we also do not want to revist nodes
+
+        queue = collections.deque()
+
+        queue.append((target, 0))
+        visited = set()
+        visited.add(target)
+
+        res = []
+
+        while queue:
+            node, distance = queue.popleft()
+
+            if distance == k:
+                res.append(node.val)
+            else:
+                for nei in connectedNodes[node]:
+                    if nei not in visited:
+                        queue.append((nei, distance + 1))
+                        visited.add(nei)
+
+        
+        return res
+
+
+
+            
+            
