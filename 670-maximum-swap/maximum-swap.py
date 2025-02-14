caab0a8f73390923aaf6092754e0f53d6c7781de
Roble <roblesiyad123@gmail.com>
2025-02-14 11:25:52 +0000

    Time: 0 ms (100.00%) | Memory: 12.5 MB (49.04%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -18,8 +18,12 @@ class Solution(object):
             if nums[i] < maxNum:
                 swap_i = i
                 swap_j = maxIndex
-        
-        nums[swap_i], nums[swap_j] = nums[swap_j], nums[swap_i]
+            
+        if swap_i != -1 and swap_j != -1:
+            nums[swap_i], nums[swap_j] = nums[swap_j], nums[swap_i]
 
         return int("".join(nums))
+
+        # Take the biggest number on the right and swap it with a smaller number on the left
+        # If not indexes are found, this wont work
  
