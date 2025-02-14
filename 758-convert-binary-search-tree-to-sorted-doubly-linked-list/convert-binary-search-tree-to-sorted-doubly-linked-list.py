6d49614ee40bba026c67a61c6420e0f76b278e13
Roble <roblesiyad123@gmail.com>
2025-02-14 08:43:17 +0000

    Time: 28 ms (90.64%) | Memory: 13.6 MB (55.61%) - LeetSync

--- 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -1,42 +1,36 @@
 """
 # Definition for a Node.
-class Node:
+class Node(object):
     def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 """
 
-class Solution:
-    def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+class Solution(object):
+    def treeToDoublyList(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
         if not root:
             return None
-        # A doubly linked list
-        # Use left as prev & right as next
-        # To get sorted order in a bst, we can use inorder traversal
-        # We can keep track of the first and last elements.
-        # We call recurivse on left subtree
-        # We need to check if we are at the first node, we can do this by checking if last element has been set
-        # Now we point the current node at the first element and the first eleemtn to this node
-        # Now we do the same to the right subtree
-        # Call this helper function
-        # Now set the first and last nodes to point at each other
-        # We can return the first node?
+        
         self.firstNode = None
         self.lastNode = None
 
-        self.convert_to_doubly_linked_list(root)
+        self.convertBinarySearchTreeToLL(root)
 
         self.firstNode.left = self.lastNode
         self.lastNode.right = self.firstNode
 
         return self.firstNode
     
-    def convert_to_doubly_linked_list(self, node):
+    def convertBinarySearchTreeToLL(self, node):
         if not node:
-            return None
-            
-        self.convert_to_doubly_linked_list(node.left)
+            return
+        
+        self.convertBinarySearchTreeToLL(node.left)
 
         if not self.lastNode:
             self.firstNode = node
@@ -46,4 +40,5 @@ class Solution:
         
         self.lastNode = node
 
-        self.convert_to_doubly_linked_list(node.right)
+        self.convertBinarySearchTreeToLL(node.right)
+
