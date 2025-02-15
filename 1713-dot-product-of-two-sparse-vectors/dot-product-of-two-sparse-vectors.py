af34a6c4305279a23f74f585c9c12209528abdff
Roble <roblesiyad123@gmail.com>
2025-02-15 23:23:59 +0000

    Time: 1390 ms (87.72%) | Memory: 16.8 MB (62.46%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.py
@@ -1,36 +1,21 @@
-"""
-We know that anything times zero, will return 0.
-When we intialise the sparseVector, create a hashmap corrisponding to index at which to multiply by.
-Is maybe a list of lists better?
-"""
 class SparseVector:
-    def __init__(self, nums: List[int]):
-        self.arr = []
+    def __init__(self, nums):
+        self.validNums = collections.defaultdict(int)
 
-        for i in range(len(nums)):
-            if nums[i]:
-                self.arr.append((i, nums[i]))
-    
+        for i, num in enumerate(nums):
+            if num:
+                self.validNums[i] = num
+        
     # Return the dotProduct of two sparse vectors
-    def dotProduct(self, vec: 'SparseVector') -> int:
-        total = 0
+    def dotProduct(self, vec):
+        product = 0
 
-        i, j = 0, 0
-
-        while i < len(self.arr) and j < len(vec.arr):
-            iIndex, iValue = self.arr[i]
-            jIndex, jValue = vec.arr[j]
-
-            if iIndex == jIndex:
-                total += (iValue * jValue)
-                i += 1
-                j += 1
-            elif iIndex < jIndex:
-                i += 1
-            else:
-                j += 1
-
-        return total
+        for key, value in self.validNums.items():
+            if key in vec.validNums:
+                product += value * vec.validNums[key]
+        
+        return product
+        
 
 # Your SparseVector object will be instantiated and called as such:
 # v1 = SparseVector(nums1)
