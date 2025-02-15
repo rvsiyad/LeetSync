96d7cb7c1c51dee9d5e01326ed0a70a5bb7651e8
Roble <roblesiyad123@gmail.com>
2025-02-15 19:18:59 +0000

    Time: 0 ms (100.00%) | Memory: 12.7 MB (11.85%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,15 +1,18 @@
-class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
-        # Use binary search, check if we are in a montoinc increasing or decreasing array
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         L = 0
         R = len(nums) - 1
 
         while L <= R:
-            m = (L + R) // 2
+            m = L + (R - L) // 2
 
             if m > 0 and nums[m] < nums[m - 1]:
                 R = m - 1
-            elif m < len(nums) - 1 and nums[m] < nums[m + 1]:
-                L = m + 1
+            elif m < len(nums) - 1 and nums[m + 1] > nums[m]:
+                L = m+1
             else:
                 return m
