ceb55fc200a6975a508cd4f346e04c8fce304706
Roble <roblesiyad123@gmail.com>
2025-02-15 20:28:46 +0000

    Time: 6 ms (1.65%) | Memory: 12.5 MB (26.82%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,24 +1,25 @@
-class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        # Use recursion
-        isNegative = n < 0
-
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
         def helper(x, n, cache):
             if n in cache:
                 return cache[n]
             
-            if n == 0:
-                return 1
-
             if n == 1:
                 return x
             
+            if n == 0:
+                return 1
+            
+            cache[n] = helper(x, n // 2, cache) * helper(x, n // 2, cache) * (x if n % 2 != 0 else 1)
 
-            cache[n] = helper(x, n // 2, cache) * helper(x, n // 2, cache) * (x if n % 2 else 1)
-    
             return cache[n]
         
-        result = helper(x, abs(n), {})
-        
-        return  1 / result if isNegative  else result
-            
+        if n >= 0:
+            return helper(x, n, {})
+        else:
+            return 1 / helper(x, abs(n), {}) 
