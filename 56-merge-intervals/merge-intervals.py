1a4d844a565277da745cdf613e2aa1953834f01b
Roble <roblesiyad123@gmail.com>
2025-02-15 23:54:23 +0000

    Time: 15 ms (26.63%) | Memory: 16.2 MB (48.65%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,23 +1,21 @@
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
         intervals.sort()
         
-        arr = []
+        stack = []
+        stack.append(intervals[0])
 
-        for i in range(len(intervals)):
-            if arr:
-                start, end = intervals[i]
-                prevStart, prevEnd = arr[-1]
+        for i in range(1, len(intervals)):
+            prevEnd = stack[-1][1]
 
-                print(prevStart)
-
-                if prevEnd >= start:
-                    arr.pop()
-                    arr.append([min(start, prevStart), max(prevEnd, end)])
-                else:
-                    arr.append(intervals[i])
-            
+            if prevEnd < intervals[i][0]:
+                stack.append(intervals[i])
             else:
-                arr.append(intervals[i])
-
-        return arr
+                prevStart, prevEnd = stack.pop()
+                stack.append([min(prevStart, intervals[i][0]), max(prevEnd, intervals[i][1])])
+        
+        return stack
