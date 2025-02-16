9beb8c6503979f17ca0c464c8d103431e804e442
Roble <roblesiyad123@gmail.com>
2025-02-16 18:56:00 +0000

    Time: 100 ms (19.46%) | Memory: 28.4 MB (35.84%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -1,22 +1,15 @@
-class Solution:
-    def findBuildings(self, heights: List[int]) -> List[int]:
+class Solution(object):
+    def findBuildings(self, heights):
         """
-        Use a monotonic decreasing stack, we keep popping from the stack if the new value we want to add is greater
-        than the previously added height. We add the indexes to the stack
-
-        Example:
-        heights = [4,2,3,1]
-        indexes = [0,1,2,3]
-
-        stack = [0,2,]
+        :type heights: List[int]
+        :rtype: List[int]
         """
         stack = []
 
         for i in range(len(heights)):
-            if stack:
-                while stack and heights[i] >= heights[stack[-1]]:
-                    stack.pop()
-
+            while stack and heights[stack[-1]] <= heights[i]:
+                stack.pop()
+            
             stack.append(i)
         
         return stack
