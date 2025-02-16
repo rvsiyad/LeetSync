9aa497bd3eccd0950b2a0d358d07fc53c575e999
Roble <roblesiyad123@gmail.com>
2025-02-16 13:47:03 +0000

    Time: 0 ms (100.00%) | Memory: 12.5 MB (13.62%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,14 +1,14 @@
-class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
+class Solution(object):
+    def nextPermutation(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
         pivot = -1
 
         for i in range(len(nums) -1, 0, -1):
             if nums[i] > nums[i - 1]:
                 pivot = i - 1
-
                 break
         
         if pivot == -1:
@@ -19,8 +19,9 @@ class Solution:
 
         while nums[swap] <= nums[pivot]:
             swap -= 1
-        
-        nums[swap], nums[pivot] = nums[pivot], nums[swap]
-        nums[pivot + 1:] = reversed(nums[pivot + 1:])
 
-        return
+        nums[swap], nums[pivot] = nums[pivot], nums[swap]
+
+        nums[pivot + 1:] = reversed(nums[pivot + 1:]) 
+
+        return nums
