c313a81faeb7496301a02d7bb12126ab05cf010b
Roble <roblesiyad123@gmail.com>
2025-02-16 12:32:40 +0000

    Time: 0 ms (100.00%) | Memory: 13.2 MB (64.32%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,51 +1,51 @@
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # Binary search
-        # Two pass? Once to find the left start and one to find the right start
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        firstElement = self.firstElementBS(nums, target)
+        lastElement = self.lastElementBS(nums, target)
+
+        return [firstElement, lastElement]
+    
+    def firstElementBS(self, nums, target):
         L = 0
         R = len(nums) - 1
 
-        # Find the target and then we can search while loop to find the left most and right most occurences?
-        
-        startIndex = self.findFirstElement(nums, target)
-        endIndex = self.findLastElement(nums, target)
-            
-        return [startIndex, endIndex]
-
-    def findFirstElement(self, nums, target):
-        L = 0
-        R = len(nums) - 1
+        index = -1
 
         while L <= R:
-            m = (L + R) // 2
+            m = L + (R - L) // 2
 
             if nums[m] == target:
-                if m == 0 or nums[m] != nums[m - 1]:
-                    return m
-                else:
-                    R = m - 1
+                index = m
+                R = m - 1
+                
             elif nums[m] > target:
                 R = m - 1
             else:
                 L = m + 1
         
-        return -1
-            
-    def findLastElement(self, nums, target):
+        return index
+
+
+    def lastElementBS(self, nums, target):
         L = 0
         R = len(nums) - 1
 
+        index = -1
+
         while L <= R:
-            m = (L + R) // 2
+            m = L + (R - L) // 2
 
             if nums[m] == target:
-                if m == len(nums) - 1 or nums[m] != nums[m + 1]:
-                    return m
-                else:
-                    L = m + 1
+                index = m
+                L = m + 1
             elif nums[m] > target:
                 R = m - 1
             else:
                 L = m + 1
         
-        return -1
+        return index
