fa7aeab3cfd5605eac34dbf589daeaa7854d6e79
Roble <roblesiyad123@gmail.com>
2025-02-20 15:16:25 +0000

    Time: 256 ms (66.19%) | Memory: 13.9 MB (30.48%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,37 +1,36 @@
 from collections import deque
-class Solution:
-    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        ROWS = len(grid)
-        COLS = len(grid[0])
 
-        if grid[0][0] == 1 or grid[ROWS - 1][COLS-1] == 1:
+class Solution(object):
+    def shortestPathBinaryMatrix(self, grid):
+        ROWS, COLS = len(grid), len(grid[0])
+
+        if grid[0][0] == 1 or grid[ROWS -1][COLS -1] == 1:
             return -1
 
         queue = deque()
-
-        queue.append((0, 0))
-        grid[0][0] = 1
-
         length = 1
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [-1, -1], [1, -1], [-1, 1]]
+
+        queue.append([0, 0])
+        visited = set()
 
         while queue:
             for _ in range(len(queue)):
                 row, col = queue.popleft()
 
-                if row == ROWS - 1 and col == COLS - 1:
+                if row == ROWS -1 and col == COLS -1:
                     return length
-                
-                directions = [[0, 1], [1, 0], [-1, 0], [0, -1], [-1, 1], [1, -1], [-1, -1], [1, 1]]
 
                 for dr, dc in directions:
                     newRow, newCol = row + dr, col + dc
 
-                    if newRow < 0 or newRow >= ROWS or newCol < 0 or newCol >= COLS or grid[newRow][newCol] == 1:
+                    if newRow < 0 or newRow >= ROWS or newCol < 0 or newCol >= COLS or (newRow, newCol) in visited or grid[newRow][newCol] == 1:
                         continue
                     
-                    grid[newRow][newCol] = 1
                     queue.append((newRow, newCol))
+                    visited.add((newRow, newCol))
             
             length += 1
+        
         
         return -1
