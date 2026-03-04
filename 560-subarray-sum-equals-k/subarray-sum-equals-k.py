3b0136fbdbbd5a785092d2ead16bb5b23bea4352
Roble <roblesiyad123@gmail.com>
2026-03-04 14:57:00 +0000

    Time: 55 ms (6.33%) | Memory: 22.1 MB (27.39%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,20 +1,17 @@
-class Solution(object):
-    def subarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        prefixCount = collections.defaultdict(int)
-        prefixSum = res = 0
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix_sum = [0] * (len(nums) + 1)
+        result = 0
+        prefix_map = {0: 1}
 
-        prefixCount[0] = 1
+        for i in range(len(nums)):
+            prefix_sum[i + 1] = prefix_sum[i] + nums[i]
 
-        for num in nums:
-            prefixSum += num
-
-            res += prefixCount[prefixSum - k]
-
-            prefixCount[prefixSum] += 1
+            if prefix_sum[i + 1] - k in prefix_map:
+                result += prefix_map[prefix_sum[i + 1] - k]
         
-        return res
+            prefix_map[prefix_sum[i + 1]] = 1 + prefix_map.get(prefix_sum[i + 1], 0)
+
+        # Now we need to go through hashmap and see depending on counts of values seen
+        
+        return result
